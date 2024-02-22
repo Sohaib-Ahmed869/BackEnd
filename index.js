cbc88d220ae99f8be4a090def27f4ff91a47f319
M-Abdullah03 <m18ab03@gmail.com>
2024-02-22 19:58:39 +0500

    stuff

--- index.js
+++ index.js
@@ -10,23 +10,30 @@ const AdminRoutes = require('./Routes/admin');
 const SuperAdminRoutes = require('./Routes/SuperAdmin');
 const CategoryRoutes = require('./Routes/category');
 const CustomerRouters = require('./Routes/user');
+const ProductRoutes = require('./Routes/product');
 
 // Connect to MongoDB
 // mongoose.connect('mongodb://127.0.0.1:27017/theWrapSpot')
 mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log('Connected to MongoDB'))
-.catch(err => console.log(err));
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.log(err));
 
 // Middleware
 app.use(express.json());
 app.use(cors());
-
+//logger
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.path}`);
+    next();
+}
+);
 
 // Import routes
 app.use('/admin', AdminRoutes);
 app.use('/superadmin', SuperAdminRoutes);
 app.use('/category', CategoryRoutes);
-app.use('/user', CustomerRouters);
+app.use('/', CustomerRouters);
+app.use('/products', ProductRoutes);
 
 
 
