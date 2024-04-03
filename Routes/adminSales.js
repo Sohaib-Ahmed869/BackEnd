c0da903427ae6765bd8d3377b46f856f0fd37350
Sohaib-Ahmed869 <i211114@nu.edu.pk>
2024-04-03 22:44:42 +0500

    Changes in adminSales Route

--- Routes/adminSales.js
+++ Routes/adminSales.js
@@ -23,7 +23,11 @@ Router.get('/orders', async (req, res) => {
 Router.get('/sales/:date', async (req, res) => {
     try {
         const date = req.params.date;
-        const orderss = await POS_Order.find({});
+        let orderss = await POS_Order.find({});
+        //filter the orders so that they are only the ones with pending and completed status
+        orderss = orderss.filter(order => {
+            return order.Status === 'Delivered' || order.Status === 'Completed';
+        });
         const orders = orderss.filter(order => {
             // Extract the date portion of the order's Date field
             const orderDate = order.Date.toISOString().split('T')[0]; // Format: YYYY-MM-DD
@@ -31,18 +35,17 @@ Router.get('/sales/:date', async (req, res) => {
             // Check if the order's date matches the provided date
             return orderDate === date;
         });
-        console.log(orders);
         let number_of_orders = orders.length;
         let total = 0;
         let cardTotal = 0;
         let cashTotal = 0;
         orders.forEach(order => {
-            total += order.Grand_Total;
+            total += order.Total;
             if (order.Payment_Method === 'Card') {
-                cardTotal += order.Grand_Total;
+                cardTotal += order.Total;
             }
-            else {
-                cashTotal += order.Grand_Total;
+            else if (order.Payment_Method === 'Cash') {
+                cashTotal += order.Total;
             }
         });
         return res.json({ Total: total, Card: cardTotal, Cash: cashTotal, Number_of_Orders: number_of_orders });
@@ -57,12 +60,32 @@ Router.get('/sales/:date', async (req, res) => {
 Router.get('/sales/month/:month', async (req, res) => {
     try {
         const month = req.params.month;
-        const orders = await POS_Order.find({ Date: { $regex: month } });
-        let total = 0;
-        orders.forEach(order => {
-            total += order.Grand_Total;
+        let orderss = await POS_Order.find({});
+        //filter the orders so that they are only the ones with pending and completed status
+        orderss = orderss.filter(order => {
+            return order.Status === 'Delivered' || order.Status === 'Completed';
         });
-        return res.json({ total });
+        const filteredOrders = orderss.filter(order => {
+            const orderMonth = order.Date.toISOString().split('-')[1]; // Extract the month portion of the order's Date field
+            return orderMonth == month;
+        });
+
+        let total = 0;
+        filteredOrders.forEach(order => {
+            total += order.Total;
+        });
+        let number_of_orders = filteredOrders.length;
+        let cardTotal = 0;
+        let cashTotal = 0;
+        filteredOrders.forEach(order => {
+            if (order.Payment_Method === 'Card') {
+                cardTotal += order.Total;
+            }
+            else if (order.Payment_Method === 'Cash') {
+                cashTotal += order.Total;
+            }
+        });
+        return res.json({ Total: total, Card: cardTotal, Cash: cashTotal, Number_of_Orders: number_of_orders });
     } catch (error) {
         console.error(error.message);
         return res.status(500).json({ error: 'Server Error' });
@@ -73,24 +96,47 @@ Router.get('/sales/month/:month', async (req, res) => {
 Router.get('/sales/year/:year', async (req, res) => {
     try {
         const year = req.params.year;
-        const orders = await POS_Order.find({ Date: { $regex: year } });
-        let total = 0;
-        orders.forEach(order => {
-            total += order.Grand_Total;
+        let orderss = await POS_Order.find({});
+        //filter the orders so that they are only the ones with pending and completed status
+        orderss = orderss.filter(order => {
+            return order.Status === 'Delivered' || order.Status === 'Completed';
         });
-        return res.json({ total });
+        const filteredOrders = orderss.filter(order => {
+            const orderYear = order.Date.toISOString().split('-')[0]; // Extract the year portion of the order's Date field
+            return orderYear == year;
+        });
+
+        let total = 0;
+        filteredOrders.forEach(order => {
+            total += order.Total;
+        });
+        let number_of_orders = filteredOrders.length;
+        let cardTotal = 0;
+        let cashTotal = 0;
+        filteredOrders.forEach(order => {
+            if (order.Payment_Method === 'Card') {
+                cardTotal += order.Total;
+            }
+            else if (order.Payment_Method === 'Cash') {
+                cashTotal += order.Total;
+            }
+        });
+        return res.json({ Total: total, Card: cardTotal, Cash: cashTotal, Number_of_Orders: number_of_orders });
     } catch (error) {
         console.error(error.message);
         return res.status(500).json({ error: 'Server Error' });
     }
 });
 
-
 // Function to calculate most sold product
 const getMostSoldProductbyDate = async (date) => {
     try {
 
-        const orderss = await POS_Order.find({});
+        let orderss = await POS_Order.find({});
+        //filter the orders so that they are only the ones with delivered and completed status
+        orderss = orderss.filter(order => {
+            return order.Status === 'Delivered' || order.Status === 'Completed';
+        });
         const orders = orderss.filter(order => {
             // Extract the date portion of the order's Date field
             const orderDate = order.Date.toISOString().split('T')[0]; // Format: YYYY-MM-DD
@@ -101,48 +147,59 @@ const getMostSoldProductbyDate = async (date) => {
 
 
         if (orders.length === 0) {
-            return null;
+            return [];
         }
 
-        const itemQuantities = {}; // Object to store item quantities
+        const itemsOverall = [];
+
 
         // Iterate over orders and items to count quantities
         orders.forEach(order => {
-            console.log(order.Items)
             order.Items.forEach(item => {
-
-                const itemId = item.item;
+                const itemId = item._id;
                 const itemQuantity = item.quantity;
+                const itemName = item.Name;
+                const itemPrice = item.Price;
 
-                console.log(itemId)
-                console.log(itemQuantity)
                 // If the item is already in the object, add the quantity
-                if (itemQuantities[itemId]) {
-                    itemQuantities[itemId] += itemQuantity;
+                if (itemsOverall[itemName]) {
+                    itemsOverall[itemName].quantity += itemQuantity;
+                    itemsOverall[itemName].sales += itemQuantity * itemPrice;
                     return;
                 }
                 else {
-                    itemQuantities[itemId] = itemQuantity;
+                    itemsOverall[itemName] = {
+                        quantity: itemQuantity,
+                        sales: itemQuantity * itemPrice,
+                        name: itemName
+                    };
                 }
             });
         });
 
+
+
+
+                
+
         // return the items sorted by quantity
         const sortedItems = [];
-        for (const item in itemQuantities) {
-            const product = await ProductModel.findById(item);
-            const name = product.Name;
+        for (const item in itemsOverall) {
             sortedItems.push({
-                name: product.Name,
-                quantity: itemQuantities[item],
-                sales: itemQuantities[item] * product.Price
+                quantity: itemsOverall[item].quantity,
+                sales: itemsOverall[item].sales,
+                name: itemsOverall[item].name
             });
         }
+
+
+        console.log(sortedItems)
         //sort the items by quantity
         sortedItems.sort((a, b) => {
             return b.quantity - a.quantity;
         });
         return sortedItems;
+
     } catch (error) {
         console.error('Error', error.message);
         return null;
@@ -171,46 +228,52 @@ const getMostSoldProductbyMonth = async (month) => {
         const orderss = await POS_Order.find({});
         const orders = orderss.filter(order => {
             // Extract the date portion of the order's Date field
-            const orderDate = order.Date.toISOString().split('T')[0]; // Format: YYYY-MM-DD
-
+            const orderDate = order.Date.toISOString().split('-')[1]; // Format: MM
             // Check if the order's date matches the provided date
             return orderDate.includes(month);
         });
 
         if (orders.length === 0) {
-            return null;
+            return [];
         }
 
-        const itemQuantities = {}; // Object to store item quantities
-
+        const itemsOverall = [];
         // Iterate over orders and items to count quantities
         orders.forEach(order => {
             order.Items.forEach(item => {
-                const itemId = item.item;
+                const itemId = item._id;
                 const itemQuantity = item.quantity;
+                const itemName = item.Name;
+                const itemPrice = item.Price;
 
-                // If the item is already in the object, add the quantity
-                if (itemQuantities[itemId]) {
-                    itemQuantities[itemId] += itemQuantity;
+                // check by name
+                if (itemsOverall[itemName]) {
+                    itemsOverall[itemName].quantity += itemQuantity;
+                    itemsOverall[itemName].sales += itemQuantity * itemPrice;
                     return;
                 }
                 else {
-                    itemQuantities[itemId] = itemQuantity;
+                    itemsOverall[itemName] = {
+                        quantity: itemQuantity,
+                        sales: itemQuantity * itemPrice,
+                        name: itemName
+                    };
                 }
-            });
+            }
+            );
+
         });
 
         // return the items sorted by quantity
         const sortedItems = [];
-        for (const item in itemQuantities) {
-            const product = await ProductModel.findById(item);
-            const name = product.Name;
+        for (const item in itemsOverall) {
             sortedItems.push({
-                name: product.Name,
-                quantity: itemQuantities[item],
-                sales: itemQuantities[item] * product.Price
+                quantity: itemsOverall[item].quantity,
+                sales: itemsOverall[item].sales,
+                name: itemsOverall[item].name
             });
         }
+
         //sort the items by quantity
         sortedItems.sort((a, b) => {
             return b.quantity - a.quantity;
@@ -246,39 +309,44 @@ const getMostSoldProduct = async () => {
             return null;
         }
 
-        const itemQuantities = {}; // Object to store item quantities
-
+        const itemsOverall = [];
         // Iterate over orders and items to count quantities
         orders.forEach(order => {
             order.Items.forEach(item => {
-                const itemId = item.item;
+                const itemId = item._id;
                 const itemQuantity = item.quantity;
+                const itemName = item.Name;
+                const itemPrice = item.Price;
 
                 // If the item is already in the object, add the quantity
-                if (itemQuantities[itemId]) {
-                    itemQuantities[itemId] += itemQuantity;
+                if (itemsOverall[itemName]) {
+                    itemsOverall[itemName].quantity += itemQuantity;
+                    itemsOverall[itemName].sales += itemQuantity * itemPrice;
                     return;
                 }
                 else {
-                    itemQuantities[itemId] = itemQuantity;
+                    itemsOverall[itemName] = {
+                        quantity: itemQuantity,
+                        sales: itemQuantity * itemPrice,
+                        name: itemName
+                    };
                 }
             });
-        });
+        }
+
+        );
 
         // return the items sorted by quantity
         const sortedItems = [];
-        for (const item in itemQuantities) {
-            const product = await ProductModel.findById(item);
-            const name = product.Name;
-            sortedItems.push(
-                {
-                    name: product.Name,
-                    quantity: itemQuantities[item],
-                    sales: itemQuantities[item] * product.Price
-                }
-            );
-
+        for (const item in itemsOverall) {
+            sortedItems.push({
+                quantity: itemsOverall[item].quantity,
+                sales: itemsOverall[item].sales,
+                name: itemsOverall[item].name
+            });
         }
+
+            
         //sort the items by quantity
         sortedItems.sort((a, b) => {
             return b.quantity - a.quantity;
